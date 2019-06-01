4904ec75c964a70549593646cd8378195c2991aa
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-01 21:08:53 +0200

    Delete user & profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -134,13 +134,32 @@ router.get('/user/:user_id', async (req, res) => {
       user: req.params.user_id
     }).populate('user', ['name', 'avatar']);
 
-    if (!profile)
-      return res.status(400).json({ msg: 'Profile not found' });
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
     res.json(profile);
   } catch (err) {
     if (err.kind == 'ObjectId') {
       return res.status(400).json({ msg: 'Profile not found' });
     }
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route       DELETE api/profile
+// @desc        Delete profile, user & post
+// @access      Private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
