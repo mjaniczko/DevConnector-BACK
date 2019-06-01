fd3ed82323be9117b3a0b1a167b6ba107818909c
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-01 19:08:24 +0200

    Start building create/update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const { check, validationResult } = require('express-validator/check');
 
 // @route       GET api/profile/me
 // @desc        Get current users profile
@@ -26,4 +27,28 @@ router.get('/me', auth, async (req, res) => {
   }
 });
 
+// @route       Post api/profile/me
+// @desc        Create or update user profile
+// @access      Private
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required')
+        .not()
+        .isEmpty(),
+      check('skills', 'Skills is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array });
+    }
+  }
+);
 module.exports = router;
