1559cc9968293e016778374aa77534250b934535
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-01 19:52:18 +0200

    Add () after errors.array

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -47,7 +47,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array });
+      return res.status(400).json({ errors: errors.array() });
     }
   }
 );
