ce328f8e6334cf3bf86093211305e9044f454cea
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-04 21:16:18 +0200

    Unlike

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -135,4 +135,35 @@ router.put('/like/:id', auth, async (req, res) => {
     res.status(500).send('Server erorr');
   }
 });
+
+// @route       PUT api/post/unlike/:id
+// @desc        Like a post
+// @access      Private
+
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Check if the post has already been liked
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' });
+    }
+
+    // Get remove index
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server erorr');
+  }
+});
 module.exports = router;
